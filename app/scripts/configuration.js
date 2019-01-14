43a153ca4999de6510fdbfbfbc10c2f3ce2a00a0
kruded <30819672+kruded@users.noreply.github.com>
2019-01-14 16:11:50 +0700

    Add files via upload

--- app/scripts/configuration.js
+++ app/scripts/configuration.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',apiEndpoint:'http://61.91.109.57:8000'})
+.constant('ENV', {name:'development',apiEndpoint:'http://61.91.109.57:8000/'})
 
 ;
